db5b58a1f6021016962fb4ab8bac9e2629dfcff2
Eundms <dms011221@gmail.com>
2020-03-23 14:22:40 +0900

    for merge with seungjoo

--- app/src/main/java/com/example/user/scheduleitssu/TabFragment1.java
+++ app/src/main/java/com/example/user/scheduleitssu/TabFragment1.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,7 +22,8 @@ public class TabFragment1 extends Fragment {
     Context context;
     RecyclerView today2doRecyclerView;
     Today2doAdapter today2doAdapter;
-
+    CalendarView todocalendar;
+    EditText selectedday;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
 
         View root=inflater.inflate(R.layout.fragment_tab1,container,false);
@@ -36,7 +41,19 @@ public class TabFragment1 extends Fragment {
         today2doArrayList.add(new Today2do("6)(default)"));
         today2doAdapter=new Today2doAdapter(this.getContext(),today2doArrayList);
         today2doRecyclerView.setAdapter(today2doAdapter);
-    return root;
+
+        selectedday=(EditText)root.findViewById(R.id.addwhichdaytodo);
+      //캘린더에서 오늘 날짜 받아오기  selectedday.setText();
+        todocalendar=(CalendarView)root.findViewById(R.id.todocalendar);
+        todocalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getContext(), ""+year+"/"+(month+1)+"/"+dayOfMonth, Toast.LENGTH_SHORT).show();
+                selectedday.setText(""+year+"/"+(month+1)+"/"+dayOfMonth);
+            }});
+
+        return root;
     }
+
 }
 
