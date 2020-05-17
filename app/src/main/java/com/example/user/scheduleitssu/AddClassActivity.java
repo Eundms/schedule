dab97d19de054accee2ba1eafe590006a6e5ef04
mihea@gmail.com <mihea12@gmail.com>
2020-05-17 21:18:43 +0900

    calendar add

--- app/src/main/java/com/example/user/scheduleitssu/AddClassActivity.java
+++ app/src/main/java/com/example/user/scheduleitssu/AddClassActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.ArrayRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,15 @@ import android.widget.Toast;
 import com.example.user.scheduleitssu.DataClass.Note;
 import com.example.user.scheduleitssu.DataClass.Subject;
 import com.example.user.scheduleitssu.DataClass.User;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Calendar;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,7 +32,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 //firebase database
@@ -34,6 +46,8 @@ import java.util.HashMap;
 public class AddClassActivity extends AppCompatActivity implements View.OnClickListener{
 Button backbtn;
 Button addbtn;
+
+String subject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,10 +89,12 @@ Button addbtn;
                         /////////////////////////////////////////////////////////////////////////////
                         /*1번째 버전*/
                         Subject sub=new Subject(subjectname.getText().toString(),subjectinfo.getText().toString(),new ArrayList<Note>());
+                        subject=subjectname.getText().toString();
                         String uid=user.getDisplayName()+"_"+user.getUid();
                         HashMap<String,Object>add_subject=new HashMap<>();
                         add_subject.put("Subject_"+sub.getClassname(),sub);
                         databaseReference.child("Student").child(uid).child("SubjectList").updateChildren(add_subject);
+                        new CalendarUtil().execute();
 
                         /*2번째 버전
                         Subject sub=new Subject(subjectname.getText().toString(),new ArrayList<Note>());
@@ -108,4 +124,57 @@ Button addbtn;
         }
 
     }
+
+    class CalendarUtil extends AsyncTask<Void, Void, String> {
+
+        HttpTransport transport = AndroidHttp.newCompatibleTransport();
+        JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+        FirebaseAuth mAuth=FirebaseAuth.getInstance();
+        FirebaseUser user=mAuth.getCurrentUser();
+        GoogleAccountCredential credential;
+        com.google.api.services.calendar.Calendar service = null;
+
+        public CalendarUtil(){
+
+
+            // Google Accounts
+            credential =
+                    GoogleAccountCredential.usingOAuth2(getApplicationContext(), Collections.singleton(CalendarScopes.CALENDAR));
+            credential.setSelectedAccountName(user.getEmail());
+
+            // Tasks client
+            service =
+                    new com.google.api.services.calendar.Calendar.Builder(transport, jsonFactory, credential)
+                            .setApplicationName("Uninote").build();
+
+        }
+
+
+        @Override
+        protected String doInBackground(Void ...voids) {
+
+            // Create a new calendar
+            com.google.api.services.calendar.model.Calendar calendar = new Calendar();
+            calendar.setSummary(subject);
+            calendar.setTimeZone("Asia/Seoul");
+
+            // 구글 캘린더에 새로 만든 캘린더를 추가
+            Calendar createdCalendar = null;
+            try {
+                createdCalendar = service.calendars().insert(calendar).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // 추가한 캘린더의 ID를 가져옴.
+            String calendarId = createdCalendar.getId();
+
+
+
+            Log.d("calendar",createdCalendar.getId());
+            return null;
+        }
+    }
+
 }
